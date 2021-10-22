e1bb399599ca53fa7c7eb22fbeeec20acf7647c4
JZD <1771887904@qq.com>
2021-10-22 17:50:18 +0800

    用类设置元素高度和背景颜色

--- js/创建类和对象.js
+++ js/创建类和对象.js
@@ -1,57 +1,34 @@
 window.addEventListener('load', function () {
     class Star {
-        constructor(uname, sex) {
-            this.uname = uname;
-            this.sex = sex;
-            this.data = [{
-                    address: "重庆",
-                    job: "前端工程师",
-                    salary: 13000
-                },
-                {
-                    address: "北京",
-                    job: "Java岗位",
-                    salary: 14000
-                },
-                {
-                    address: "上海",
-                    job: "Golang开发",
-                    salary: 18000
-                },
-                {
-                    address: "杭州",
-                    job: "数据分析师",
-                    salary: 12000
-                },
-                {
-                    address: "深圳",
-                    job: "全栈工程师",
-                    salary: 23000
-                }
-
-            ]
+        constructor(obj) {
+            this.obj = obj;
+            this.data = {
+                color: ['red', 'pink', 'blue', 'green', 'skyblue', 'deeppink'],
+                sideWidth: [200, 250, 300, 350, 400, 450]
+            };
+        };
+        setColor = function (i) {
+            this.obj.style.backgroundColor = this.data.color[i]
+        };
+        set_width_height = function (i) {
+            this.obj.style.width = this.data.sideWidth[i] + 'px'
+            this.obj.style.height = this.data.sideWidth[i] + 'px'
+        };
+        creat = function (object_element){
+            let element = document.createElement(object_element);
+            this.obj.appendChild(element)
+        };
+    }
+    class animate  extends Star{
+        constructor() {
+            super(data);
+            this.data = data
         }
     }
-    const ldh = new Star("李德华", 18);
-    console.log(ldh.uname, ldh.sex);
-    const box = document.querySelector("#box");
-    let box_li = null,
-        box_spanOne = null,
-        box_spanTwo = null,
-        box_spanThree;
+    const box = document.querySelector('#box');
+    const box_obj = new Star(box)
+    box_obj.set_width_height(1)
+    box_obj.setColor(5)
+    box_obj.creat('div')
 
-    for (let i = 0; i < ldh.data.length; i++) {
-        box_li = this.document.createElement('li')
-        box_spanOne = this.document.createElement('span')
-        box_spanTwo = this.document.createElement('span')
-        box_spanThree = this.document.createElement('span')
-        box_spanOne.textContent = ldh.data[i].address;
-        box_spanTwo.textContent = ldh.data[i].job;
-        box_spanThree.textContent = ldh.data[i].salary;
-
-        box_li.appendChild(box_spanOne)
-        box_li.appendChild(box_spanTwo)
-        box_li.appendChild(box_spanThree)
-        box.children[0].appendChild(box_li)
-    }
 })
