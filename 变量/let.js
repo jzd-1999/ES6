f53ee517fd86294f264bdde7c63dc3cf3bcf00a1
JZD <1771887904@qq.com>
2021-10-27 10:55:15 +0800

    变量

---
+++ 变量/let.js
@@ -0,0 +1,11 @@
+let a;
+let b,c,d;
+let e =100;
+let f = 521,g = 'hhh',h = [];
+//变量不能重复声明
+// let star = '罗志祥';
+// let star = '李云迪';
+var star_1 = "罗志祥";
+var star_1 = "李云迪"
+console.log(star_1);
+//不存在变量提升
