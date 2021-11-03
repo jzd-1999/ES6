e2f454e9cd507b695e09c545cf49181e9ea64c85
JZD <1771887904@qq.com>
2021-11-03 20:31:59 +0800

    递归完

--- 递归/递归取数据.js
+++ 递归/递归取数据.js
@@ -5,16 +5,17 @@ const data = [{
         [{id: 1, name: "Huawei Mate40 Pro"},
             {id: 2, name: "Huawei P40 Pro"},
             {id: 3, name: "iphone13 Max Pro"},
-            {id: 4, name: "iphone13 Pro"}]
+            {id: 4, name: "iphone13 Pro"},
+            {id: 5, name: "iphone13"}]
 }]
 
 function getID(json, id) {
     let o = {}
     json.forEach(function (item) {
-        if (item.id === id){
+        if (item.id === id) {
             o = item;
-        }else if (item.goods && item.goods.length>0){
-            getID(item.goods,id);
+        } else if (item.goods && item.goods.length > 0) {
+            o = getID(item.goods, id);
         }
     });
     return o;
